8ba2b25885d013115c2758254a706781311a599d
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2024-10-28 17:36:53 +0100

    Depurado el código

--- romanos.pl
+++ romanos.pl
@@ -1,31 +1,56 @@
+% Predicado para calcular el valor de las unidades en una lista completa con cortes
+% Caso Base
+valor_unidades([], 0).
 
+% Casos para cada símbolo en la lista (con cortes)
+valor_unidades([i | Resto], Suma) :-
+    valor_unidades(Resto, SumaResto),
+    Suma is 1 + SumaResto, !.
+valor_unidades([v | Resto], Suma) :-
+    valor_unidades(Resto, SumaResto),
+    Suma is 5 + SumaResto, !.
+valor_unidades([x | Resto], Suma) :-
+    valor_unidades(Resto, SumaResto),
+    Suma is 10 + SumaResto, !.
 
-%CasoBase
-valor_unidades([],0).
+% Casos de combinaciones especiales de unidades (con cortes)
+valor_unidades([i, v | Resto], Suma) :-
+    valor_unidades(Resto, SumaResto),
+    Suma is 4 + SumaResto, !.
+valor_unidades([i, x | Resto], Suma) :-
+    valor_unidades(Resto, SumaResto),
+    Suma is 9 + SumaResto, !.
 
-%CasosComplejos
-valor_unidades([i | _], 1) :- !.
-valor_unidades([i, i | _], 2) :- !.
-valor_unidades([i, i, i | _], 3) :- !.
-valor_unidades([i, v | _], 4) :- !.
-valor_unidades([v | _], 5) :- !.
-valor_unidades([v, i | _], 6) :- !.
-valor_unidades([v, i, i | _], 7) :- !.
-valor_unidades([v, i, i, i | _], 8) :- !.
-valor_unidades([i, x | _], 9) :- !.
-% valor_unidades([x | Resto],10) :-!.
+% Predicado para calcular el valor de las decenas
+% Caso Base
+valor_decenas([], 0).
 
-%CBase
-valor_decenas([],[], 0).
-%Ccomplejos
-valor_decenas([x | Resto],Resto,10):- !.
-valor_decenas([x,x | Resto],Resto,20):- !.
-valor_decenas([x,x,x | Resto],Resto,30):- !.
-valor_decenas([x,l | Resto ],Resto,40):- !.
-valor_decenas([l | Resto], Resto, 50) :- !.
-valor_decenas([l, x | Resto], Resto, 60) :- !.
-valor_decenas([l, x, x | Resto], Resto, 70) :- !.
-valor_decenas([l, x, x, x | Resto], Resto, 80) :- !.
-valor_decenas([x, c | Resto], Resto, 90):- !.
-
+% Casos de Decenas (sin cortes)
+valor_decenas([x, x, x | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 30 + SumaResto.
+valor_decenas([x, x | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 20 + SumaResto.
+valor_decenas([x | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 10 + SumaResto.
+valor_decenas([x, l | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 40 + SumaResto.
+valor_decenas([l | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 50 + SumaResto.
+valor_decenas([l, x | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 60 + SumaResto.
+valor_decenas([l, x, x | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 70 + SumaResto.
+valor_decenas([l, x, x, x | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 80 + SumaResto.
+valor_decenas([x, c | Resto], Suma) :-
+    valor_decenas(Resto, SumaResto),
+    Suma is 90 + SumaResto.
 
