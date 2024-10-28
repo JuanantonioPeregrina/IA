b995f0fb4da592bcaceb3067bb2c11d821ddb346
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2024-10-28 12:48:18 +0100

    Corregido sintaxis

--- romanos.pl
+++ romanos.pl
@@ -1,18 +1,19 @@
+
+
 %CasoBase
 valor_unidades([],0).
 
 %CasosComplejos
-
-valor_unidades([i | Resto],1) :- !.
-valor_unidades([i,i | Resto],2) :- !.
-valor_unidades([i,i,i | Resto],3) :- !.
-valor_unidades([i,v | Resto],4) :- !.
-valor_unidades([v | Resto],5) :- !.
-valor_unidades([v,i | Resto],6) :- !.
-valor_unidades([v,i,i | Resto],7) :- !.
-valor_unidades([v,i,i,i | Resto],8) :- !.
-valor_unidades([i,x | Resto],9) :-!.
-#valor_unidades([x | Resto],10) :-!.
+valor_unidades([i | _], 1) :- !.
+valor_unidades([i, i | _], 2) :- !.
+valor_unidades([i, i, i | _], 3) :- !.
+valor_unidades([i, v | _], 4) :- !.
+valor_unidades([v | _], 5) :- !.
+valor_unidades([v, i | _], 6) :- !.
+valor_unidades([v, i, i | _], 7) :- !.
+valor_unidades([v, i, i, i | _], 8) :- !.
+valor_unidades([i, x | _], 9) :- !.
+% valor_unidades([x | Resto],10) :-!.
 
 %CBase
 valor_decenas([],[], 0).
@@ -25,6 +26,6 @@ valor_decenas([l | Resto], Resto, 50) :- !.
 valor_decenas([l, x | Resto], Resto, 60) :- !.
 valor_decenas([l, x, x | Resto], Resto, 70) :- !.
 valor_decenas([l, x, x, x | Resto], Resto, 80) :- !.
-valor_decenas([x,c | Resto], Resto,90): -!.
+valor_decenas([x, c | Resto], Resto, 90):- !.
 
 
